e1cff2722813be1b0e8392e36014f5701737185a
CukiD <40711562+Tektek9@users.noreply.github.com>
2024-03-12 04:24:36 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from PyQt5 import QtWidgets
+import sys
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+    
+if __name__ == "__main__":
+    main()
